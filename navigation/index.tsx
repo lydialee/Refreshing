58a6240bb7e5b8cf897cb323257dcf106276695f
lydia li <lydia.li@shopify.com>
2020-06-10 19:56:26 -0700

    add nav/index

---
+++ navigation/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import Home from "screens/Home";
+
+export default function index() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
